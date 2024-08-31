b7f9ecb86d7c689ed42739b4cfd50dbd21b8691c
Yohana <yohana.mfaria@gmail.com>
2024-08-31 20:22:55 -0300

    Implementando rotas

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,39 +1,34 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
-import Cadastro from "./pages/cadastro/Cadastro"
-import Login from "./pages/login/Login"
-import { AuthProvider } from "./contexts/AuthContext"
+import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+
 
 function App() {
-  // Código TypeScript
-
   return (
-    // Código TSX => HTML + CSS
     <>
-      {/* 
-          Envolvemos todos os Componentes inseridos no Componente App, 
-          com o Componente AuthProvider. 
-          Desta forma, todos os Componentes dentro dele, terão acesso 
-          aos Estados e Funções guardados na context.
-      */}
-      <AuthProvider>
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className="min-h-[80vh]">
+          <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-      </AuthProvider>
+        </AuthProvider>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
